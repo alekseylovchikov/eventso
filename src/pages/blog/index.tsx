cd4311fdf898d1034900015c436ae01af173a1b0
Aleksey Lovchikov <svdmusic@gmail.com>
2023-08-26 14:53:18 +0300

    Add page extensions

--- src/pages/blog/index.tsx
+++
@@ -1,5 +0,0 @@
-const BlogPage = () => {
-  return <div>Welcome to out blog</div>;
-};
-
-export default BlogPage;
--- src/pages/team.tsx
+++
@@ -1,7 +0,0 @@
-import { BlitzPage } from '@blitzjs/next';
-
-const TeamPage: BlitzPage = () => {
-  return <div>this is about out team</div>;
-};
-
-export default TeamPage;
